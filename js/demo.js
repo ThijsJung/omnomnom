f14d2adf7f42eca61008aaeb84a1f59d19f1e91d
Thijs Jung <thijs.jung@trivago.com>
2020-02-02 19:40:44 +0100

    Start filling DOM with recipe data when DOM loaded

--- js/demo.js
+++ js/demo.js
@@ -1,13 +1,17 @@
-var baseUrl = 'https://www.thijsjung.nl/omnomnom/';
+const baseUrl = 'https://www.thijsjung.nl/omnomnom/';
 var urlParams = new URLSearchParams(window.location.search);
 var recipeId = urlParams.get('recipe_id');
 var recipeData = null;
 
 var default_hungry_people_count = 4;
 
+document.addEventListener('DOMContentLoaded', function () {
+    get_recipe_data(recipeId);
+});
+
 function load_recipe(recipe_data){
     recipeData = recipe_data;
-    fill_recipe_description(recipe_data.title, recipe_data.description);
+    fill_recipe_description(recipe_data.title, recipe_data.description)
     fill_ingredients(recipe_data.ingredients, recipe_data.portion_size, default_hungry_people_count);
     fill_preparation(recipe_data.preparation);
     if('pro_tips' in recipe_data){
@@ -31,6 +35,7 @@ function fill_recipe_description(title, description){
 function get_and_fill_ingredients(recipeId, hungry_people_count){
     var url = baseUrl + 'recipes/' + recipeId + ".json";
     callAPI(url, fill_ingredients);
+}
 
 function recalculate_ingredients(hungry_people_count){
     fill_ingredients(recipeData.ingredients, recipeData.portion_size, hungry_people_count);
@@ -39,17 +44,17 @@ function recalculate_ingredients(hungry_people_count){
 function fill_ingredients(ingredients, portion_size, hungry_people_count){
     let ratio = hungry_people_count / portion_size;
     document.getElementById("ingredients_list").innerHTML = "";
-    for (var i = 0; i < ingredients.length; i++) {
-        var ingredient = ingredients[i];
-        var unit = ingredient.unit;
-        var normalized_quantity = ratio * ingredient.quantity;
-        var text = normalized_quantity + " " + ingredient.name;
+    for (let i = 0; i < ingredients.length; i++) {
+        let ingredient = ingredients[i];
+        let unit = ingredient.unit;
+        let normalized_quantity = ratio * ingredient.quantity;
+        let text = normalized_quantity + " " + ingredient.name;
         if (ingredient.unit){
             text = ingredient.name + ", " + normalized_quantity + " " + unit;
         }
-        var li = document.createElement("li");
+        let li = document.createElement("li");
         li.className = "ingredient";
-        var t = document.createTextNode(text);
+        let t = document.createTextNode(text);
         li.appendChild(t);
         document.getElementById("ingredients_list").appendChild(li);
     }
@@ -99,41 +104,6 @@ function get_recipe_data(recipe_id){
     callAPI(url, load_recipe);
 }
 
-function load_recipe_list(){
-    var url = baseUrl + 'recipes/recipes.json';
-    callAPI(url, function(response){
-        var recipes = response;
-        for (var i = 0; i < recipes.length; i++) {
-            var recipe = recipes[i];
-            var li = document.createElement("li");
-            var a = document.createElement("a");
-            a.href = baseUrl + 'recipe.html?recipe_id=' + recipe.id;
-            a.textContent = recipe.name;
-            li.appendChild(a);
-            document.getElementById("recipe_list").appendChild(li);
-        }
-    });
-}
-
-function fill_period_name(){
-    var today = new Date();
-    var hour = today.getHours();
-    var period_name = "snack";
-    if( hour > 8 && hour < 13 ){
-        period_name = "brunch";
-    }else if( hour >= 13 && hour < 17){
-        period_name = "lunch";
-    }else if( hour >= 17 && hour < 22){
-        period_name = "dinner";
-    }else if( 22>= hour || hour < 5){
-        period_name = "midnight snack";
-    }
-    var elements = document.getElementsByClassName("period_name");
-    Array.prototype.forEach.call(elements, function(el) {
-        el.textContent = period_name;
-    });
-}
-
 function copyToClipboard(){
     // Copy invisible text by creating a temporary textArea.
     let ingredients = document.getElementsByClassName("ingredient");
@@ -158,4 +128,3 @@ function copyToClipboard(){
     document.execCommand("copy");
     document.body.removeChild(textArea);
 }
-get_recipe_data(recipeId);
